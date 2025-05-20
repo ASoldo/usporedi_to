3c57ca07011474fae648b540b2e8db233c3a39c1
ASoldo <soldo.andrej@gmail.com>
2025-05-20 22:54:23 +0200

    Minor fix

--- components/Pwa.vue
+++ components/Pwa.vue
@@ -1,63 +1,3 @@
-<!-- <template> -->
-<!--   <div> -->
-<!--     <ClientOnly> -->
-<!--       <div v-if="$pwa?.offlineReady || $pwa?.needRefresh" class="pwa-toast" role="alert"> -->
-<!--         <div class="message"> -->
-<!--           <span v-if="$pwa.offlineReady"> -->
-<!--             App ready to work offline -->
-<!--           </span> -->
-<!--           <span v-else> -->
-<!--             New content available, click on reload button to update. -->
-<!--           </span> -->
-<!--         </div> -->
-<!--         <button v-if="$pwa.needRefresh" @click="$pwa.updateServiceWorker()"> -->
-<!--           Reload -->
-<!--         </button> -->
-<!--         <button @click="$pwa.cancelPrompt()"> -->
-<!--           Close -->
-<!--         </button> -->
-<!--       </div> -->
-<!--       <div v-if="$pwa?.showInstallPrompt && !$pwa?.offlineReady && !$pwa?.needRefresh" class="pwa-toast" role="alert"> -->
-<!--         <div class="message"> -->
-<!--           <span> -->
-<!--             Install PWA -->
-<!--           </span> -->
-<!--         </div> -->
-<!--         <button @click="$pwa.install()"> -->
-<!--           Install -->
-<!--         </button> -->
-<!--         <button @click="$pwa.cancelInstall()"> -->
-<!--           Cancel -->
-<!--         </button> -->
-<!--       </div> -->
-<!--     </ClientOnly> -->
-<!--   </div> -->
-<!-- </template> -->
-<!-- <script setup> -->
-<!-- // If you want to use it in setup, import from the nuxtApp. -->
-<!-- const { $pwa } = useNuxtApp() -->
-<!---->
-<!-- const toast = useToast() -->
-<!---->
-<!-- onMounted(() => { -->
-<!--   if ($pwa.offlineReady) -->
-<!--     toast.success('App ready to work offline') -->
-<!-- }) -->
-<!-- </script> -->
-<!---->
-<!-- <template> -->
-<!--   <!-- You can use $pwa directly in templates! --> -->
-<!--   <div v-show="$pwa.needRefresh"> -->
-<!--     <span> -->
-<!--       New content available, click on reload button to update. -->
-<!--     </span> -->
-<!---->
-<!--     <button @click="$pwa.updateServiceWorker()"> -->
-<!--       Reload -->
-<!--     </button> -->
-<!--   </div> -->
-<!-- </template> -->
-
 <template>
   <VitePwaManifest />
   <!-- <div v-if="$pwa?.offlineReady || $pwa?.needRefresh" -->
@@ -103,7 +43,7 @@
   <div v-show="$pwa?.needRefresh">
     <span> New content available, click on reload button to update. </span>
 
-    <button @click="$pwa.updateServiceWorker()">Reload</button>
+    <button @click="$pwa?.updateServiceWorker()">Reload</button>
   </div>
 </template>
 
